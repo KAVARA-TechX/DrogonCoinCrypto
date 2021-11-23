bce2d847c934a00f406b1362981050d380326cd5
KAVARA-TecX <kavara.tech@gmail.com>
2021-11-24 00:55:43 +0530

    added responsive container

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -26,44 +26,46 @@ const Home = () => {
             {/* Donations */}
             {/* <Donations /> */}
             <div className="mt-5 mb-5 pt-5 pb-5 bgImage">
-                <div className="text-center">
-                    <h4 >Why Drogon?</h4>
-                    <h2>JOIN THE HEAT</h2>
-                    <p>Drogon is the place where anyone can share memes and earn crypto!!!
-                        <br />It will forever change the way of investment we invest in crypto
-                    </p>
-                </div>
-                <div className="container-fluid mt-5 mb-5">
-                    <div className="row">
-                        <div className="col-md-1"></div>
-                        <div className="col-md-1"></div>
-                        <div className="col-md-2">
+                <div className="sd">
+                    <div className="text-center">
+                        <h4 style={{color:'#fff'}}>Why Drogon?</h4>
+                        <h2 style={{color:'#fff'}}>JOIN THE HEAT</h2>
+                        <p style={{color:'#fff'}}>Drogon is the place where anyone can share memes and earn crypto!!!
+                            <br />It will forever change the way of investment we invest in crypto
+                        </p>
+                    </div>
+                    <div className="container-fluid mt-5 mb-5">
+                        <div className="row">
+                            <div className="col-md-1"></div>
+                            <div className="col-md-1"></div>
+                            <div className="col-md-2 centerContent">
                                 <div className="card circular circularImgAnimation">
                                     <div className="card-body" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
-                                        <img src={rupee} width="90px" height="90px" alt="rupee" />
+                                        <img src={rupee} className="circularImg" alt="rupee" />
                                     </div>
                                     <p className="text-center">SHARE MEME EARN REAL CRYPTO</p>
                                 </div>
-                        </div>
-                        <div className="col-md-1"></div>
-                        <div className="col-md-2">
+                            </div>
+                            <div className="col-md-1"></div>
+                            <div className="col-md-2 centerContent">
                                 <div className="card circular circularImgAnimation1">
                                     <div className="card-body" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
-                                        <img src={wallet}  width="90px" height="90px" alt="rupee" />
+                                        <img src={wallet}  className="circularImg" alt="rupee" />
                                     </div>
                                     <p className="text-center">HOLDING REWARDS</p>
                                 </div>
-                        </div>
-                        <div className="col-md-1"></div>
-                        <div className="col-md-2">
-                            <div className="card circular circularImgAnimation">
-                                <div className="card-body" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
-                                    <img src={rocket} width="90px" height="90px" alt="rupee" />
-                                </div>
-                                <p className="text-center">TO THE MOON</p>
                             </div>
+                            <div className="col-md-1"></div>
+                            <div className="col-md-2 centerContent">
+                                <div className="card circular circularImgAnimation">
+                                    <div className="card-body" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
+                                        <img src={rocket}  className="circularImg" alt="rupee" />
+                                    </div>
+                                    <p className="text-center">TO THE MOON</p>
+                                </div>
+                            </div>
+                            <div className="col-md-1"></div>
                         </div>
-                        <div className="col-md-1"></div>
                     </div>
                 </div>
             </div>
